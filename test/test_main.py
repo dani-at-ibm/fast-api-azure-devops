4e36faf353e31b465a044f5ef89b9e4a16cedac3
Daniel Fesalbon <daniel.fesalbon@ibm.com>
2023-01-12 18:07:19 +0800

    sample test

--- test/test_main.py
+++ test/test_main.py
@@ -1,2 +1,2 @@
 def test_hello_world():
-    assert "hello world from devops." == "hello world from devops..", "Should be hello world from devops.."
+    assert "hello world from devops." == "hello world from devops.", "Should be hello world from devops.."
